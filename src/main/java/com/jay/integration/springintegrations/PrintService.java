88f9c0543e3ff5105339b85b0d9387ada7547f4f
Jatinder Singh <jpsjatinder@gmail.com>
2019-01-26 21:41:00 +0530

    Publish Subscribe Channel

--- src/main/java/com/jay/integration/springintegrations/PrintService.java
+++ src/main/java/com/jay/integration/springintegrations/PrintService.java
@@ -3,17 +3,13 @@ package com.jay.integration.springintegrations;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.Message;
 
 public class PrintService {
 	private static final Logger LOG = LoggerFactory.getLogger(PrintService.class);
 	
-	public Message<?> compute(Message<String> message) {
+	public void compute(Message<String> message) {
 		
-		throw new RuntimeException("Failed.");
-		/*LOG.info(message.getPayload());
-		int messageNumber = (int)message.getHeaders().get("messageNumber");
-		return MessageBuilder.withPayload("Message Send to output channel By MessageBuilder "+ messageNumber).build();*/
+		LOG.info(message.getPayload());
 	}
 }
